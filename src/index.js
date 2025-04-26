449f3fc61e8e6e9cc10006eeaa6953081d346fd3
ZBS-1910 <zameer1910basha@gmail.com>
2025-04-26 22:36:41 +0530

    made proxy complient

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/bookingService/api', apiroutes);
 app.use('/api', apiroutes);
 
 app.listen(ServerConfig.PORT, () => {
